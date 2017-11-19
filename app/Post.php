726771bf1aa632d375a6cfc9e62f9ace21ee226d
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-11-19 16:29:06 +0000

    cleanup

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Media;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -15,19 +16,47 @@ class Post extends Model
         'posted_at'
     ]; 
 
+    /*
+        Relationships
+     */
+
     public function source()
     {
         return $this->belongsTo('App\Source');
     }
+
     public function media()
     {
       return $this->hasMany('App\Media');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
+
+    /*
+        Utility Functions
+     */ 
     public static function uid_exists($uid)
     {
         return Static::where('uid',$uid)->count() > 0;
     }
 
+    public static function uidExists($uid)
+    {
+        return Static::where('uid',$uid)->count() > 0;
+    }
+
+
+    /*
+        Images and Media
+     */
+
+    /**
+     * Get appropriate Image (choose between cache and original)
+     * @return (string) Image Location
+     */
     public function image()
     {
         // if cache exists, return cache, 
@@ -41,23 +70,22 @@ class Post extends Model
         // otherwise, no image exists;
         return null;
     }
-
-    public function cacheImage($value='')
+    
+    /**
+     * Cache original image if cache doesn't exist
+     * @return null 
+     */
+    public function cacheImage($days = 21)
     {
+
+        // ignore caching posts that are older than $days;
+        if ($this->posted_at->diffInDays() > $days ) {
+            return ;
+        }
         if ($this->media->count() == 0) {
             $image = Media::createFromImage($this->original_image, 'post');
             $image->post_id = $this->id;
             $image->save();
         }
-    }
-
-    public function tags()
-    {
-        return $this->belongsToMany('App\Tag');
-    }
-
-    public static function uidExists($uid)
-    {
-        return Static::where('uid',$uid)->count() > 0;
     }
 }
