f27810c4b471286ec7887a21b4d22f2faba07b97
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-12-25 21:31:37 +0000

    cleaning up the route file

--- routes/web.php
+++ routes/web.php
@@ -4,28 +4,6 @@ use App\Post;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
-
-Route::get('/', function(){
-    $posts = Post::with(['Media','Source'])->orderBy('posted_at','desc')->take(50)->get();
-    return view('home')->with(compact('posts'));
-});
-
-// Authentication Routes...
-$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-$this->post('login', 'Auth\LoginController@login');
-$this->post('logout', 'Auth\LoginController@logout')->name('logout');
-
-Route::prefix('admin')->middleware('auth')->group(function(){
-    Route::resource('source', 'AdminSourceController', ['as' => 'admin'])->except('show');
-    Route::resource('tag', 'AdminTagController',['as' => 'admin'])->except('show');
-    Route::get('/', 'AdminController@index');
-});
-
-/*
-|--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 */
@@ -35,5 +13,34 @@ Route::prefix('api')->group(function(){
     Route::get('source', function(){
         return App\Source::all();
     })->middleware('auth');
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', function(){
+    $posts = Post::with(['Media','Source'])->orderBy('posted_at','desc')->take(50)->get();
+    return view('home')->with(compact('posts'));
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Auth and Admin Routes
+|--------------------------------------------------------------------------
+*/
+
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::prefix('admin')->middleware('auth')->group(function(){
+    Route::resource('source', 'AdminSourceController', ['as' => 'admin'])->except('show');
+    Route::resource('tag', 'AdminTagController',['as' => 'admin'])->except('show');
+    Route::get('/', 'AdminController@index');
 });
 
