bbdb4fb35e3abc576b419305092e36176f6e124c
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-12-31 13:03:39 +0000

    removed Laravel built in api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,2 @@
 <?php
 
-Route::prefix('api')->group(function(){
-    Route::get('posts/{count?}/{order?}', 'PostsApiController@index');
-    Route::get('source', function(){
-        return App\Source::all();
-    })->middleware('auth');
-});
