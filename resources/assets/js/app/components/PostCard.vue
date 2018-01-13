4b511633222068308fa3da1ea474930d1659a6f4
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2018-01-13 13:23:37 +0000

    using css for font sizes

--- resources/assets/js/app/components/PostCard.vue
+++ resources/assets/js/app/components/PostCard.vue
@@ -1,15 +1,18 @@
 <template>
-  <div  v-bind:class="{ 'lb-card' : true, 'lb-card--no-image': !hasImage, 'lb-card--small': this.small == 'true' }">
-      <figure class="image is-3by2" v-if="this.post.media.length > 0" style="border-bottom:1px solid silver">
-        <img :src="'/img/media/' + this.post.media[0].pointer" alt="Placeholder image">
-      </figure>
-      <div class="lb-card__section">
-       <h1>
-         {{post.short_title}}
-       </h1> 
-        <h2>
-          {{post.source.name}}
-        </h2>
+  <div class="card-aspect-ratio">
+      <div class="lb-card">
+        <figure class="image is-3by2" style="border-bottom:1px solid silver">
+          <img v-if="hasImage" :src="'/img/media/' + this.post.media[0].pointer" alt="Placeholder image">
+          <img v-else :src="'/img/placeholder_600x400.svg'" alt="Placeholder image">
+        </figure>
+        <div class="lb-card__section">
+         <h1>
+           {{post.short_title}}
+         </h1> 
+          <h2>
+            {{post.source.name}}
+          </h2>
+        </div>
       </div>
   </div>
 </template>
